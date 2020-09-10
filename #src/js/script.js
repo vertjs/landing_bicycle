f16ef1a8a0664369984020cab9eb2ef594f7054c
vertjs <elena.viktorovna.bogatyreva@gmail.com>
2020-09-10 23:32:17 +0300

    shop__gallery

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,7 @@
+let slickDots = document.querySelector(".slick-dots")
+let btn = slickDots.getElementsByTagName("li")
+
+
 let ready = () => {
 	document.querySelectorAll(".ibg").forEach(el => {
 		if(el.querySelector('img')){
@@ -5,6 +9,25 @@ let ready = () => {
 			el.querySelector('img').style.display = 'none';
 		}
 	});
+
+	document.querySelector(".body__item").style.display = 'block';
+	
+	Array.from(btn).forEach(el => {
+		el.addEventListener('click', following)
+	})
+
+}
+
+const slides = document.querySelectorAll(".body__item")
+
+
+function following(e) {
+	slickDots.querySelectorAll("li").forEach(el => el.classList.remove("active"))
+	slides.forEach(el => el.style.display = "none")
+	
+	e.target.classList.add("active")
+	
+	slides[e.target.innerText].style.display = "block"
 }
 
 document.addEventListener("DOMContentLoaded", ready);
